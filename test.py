9723dd42747730b4e3d5d403268a8ae07196eab0
ANUBOTHU ARAVIND <aanubothu@gmail.com>
2025-01-23 12:14:34 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print(22/7)
+num1 = 15  # Corrected the syntax error; removed the space between 1 and 5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
